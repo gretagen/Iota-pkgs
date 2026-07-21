ed52fca7a3707900329d5af088970613b3423bca
gretagen <besternet91@gmail.com>
2026-07-21 12:45:19 +0200

    Add stubs for ncurses, sqlite, gdbm, zlib, xz, libffiReal; fix expat stub

--- packages/ex/expat/package.nix
+++ packages/ex/expat/package.nix
@@ -2,9 +2,5 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "expat";
   version = "2.8.2";
-  src = fetchurl {
-    url = "https://example.org/expat-${finalAttrs.version}.tar.gz";
-    hash = "sha256-dummy";
-  };
   installPhase = "true";
 })
